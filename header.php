ba3755afeea1eea7465f155a70d537f39b059588
codentricks <sonzoy@gmail.com>
2021-04-02 17:31:04 +0530

    created header,footer and update index.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-US" prefix="og: http://ogp.me/ns#">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<title><?php if(is_home()) { echo bloginfo("name");  } else { echo wp_title(" | ", false, right); echo bloginfo("name"); } ?></title>
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<?php  $settings = get_option( "sp_theme_settings" );?>
+	<?php if ( get_theme_mod( 'sp_logo' ) ) : ?>
+	<link rel="shortcut icon" href="<?php echo get_theme_mod( 'sp_favicon' ); ?>" type="image/x-icon">
+    <link rel="icon" href="<?php echo get_theme_mod( 'sp_favicon' ); ?>" type="image/x-icon">
+    <?php endif; ?>
+	
+	<?php wp_head(); ?>
+
+</head>
+<body >
--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+/**
+ * The main template file
+ */
+get_header(); ?>
+
+
+
+
+<?php
+get_footer();
+?>
